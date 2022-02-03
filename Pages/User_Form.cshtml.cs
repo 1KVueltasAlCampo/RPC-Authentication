157808c7a26a558e40852661736106eb487c9355
Juan David Cruz Garcia <angrybirdsgallo@gmail.com>
2022-02-03 12:04:26 -0500

    Created user form example

---
+++ Pages/User_Form.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RPC_Authentication.Model;
+
+namespace RPC_Authentication
+{
+    public class User_FormModel : PageModel
+    {
+        [BindProperty]
+        public User user { get; set; }
+        public void OnGet()
+        {
+        }
+    }
+}
